fbb3c7f0fa77758bad78beb2248817073dfa8ac4
Joel <joel.cescit@gmail.com>
2021-03-22 11:51:16 +0100

    Login y registro de usuario y empresa acabado

--- front-end-proyecto3/src/components/AppBar.vue
+++ front-end-proyecto3/src/components/AppBar.vue
@@ -6,12 +6,23 @@
       src="https://cdn.pixabay.com/photo/2017/02/15/00/48/logo-2067396_960_720.png"
     ></v-img>
     <v-spacer></v-spacer>
-    <AuthenticationModal :dialog="dialogLogin" v-on:cerrarModal="cerrarModal"></AuthenticationModal>
-    <v-btn class="ma-1" @click="redirigirAdminPanel" plain> Panel de administrador</v-btn>
-    <v-btn class="ma-1" @click="dialogLogin = true" plain> Log In / Sign Up</v-btn>
+    <AuthenticationModal
+      :dialog="dialogLogin"
+      v-on:cerrarModal="cerrarModal"
+    ></AuthenticationModal>
+    <v-btn class="ma-1" @click="redirigirAdminPanel" plain>
+      Panel de administrador</v-btn
+    >
+    <v-btn class="ma-1" @click="dialogLogin = true" plain v-if="loged == false">
+      Log In / Sign Up</v-btn
+    >
+    <v-btn class="ma-1" @click="logOut()" plain v-if="loged == true">
+      Log Out</v-btn
+    >
   </v-app-bar>
 </template>
 <script>
+//import axios from 'axios';
 import AuthenticationModal from "./AuthenticationModal.vue";
 export default {
   name: "AppBar",
@@ -20,16 +31,35 @@ export default {
   },
   data() {
     return {
-      dialogLogin:false
+      dialogLogin: false,
+      loged: false,
     };
   },
-  methods:{
-    cerrarModal(){
-      this.dialogLogin=false;
+  methods: {
+    cerrarModal() {
+      this.dialogLogin = false;
     },
-    redirigirAdminPanel(){
-      this.$router.push("/adminPanel")
-    }
-  }
+    redirigirAdminPanel() {
+      this.$router.push("/adminPanel");
+    },
+    logOut() {
+      localStorage.removeItem("accessToken");
+      this.$router.push("/portada");
+      this.loged=false;
+    },
+    checkUser() {
+      if (localStorage.getItem("accessToken") != null) this.loged = true;
+      else {
+        this.loged = false;
+        this.$router.push("/portada");
+      }
+    },
+  },
+  created() {
+    this.checkUser();
+  },
+  updated() {
+    this.checkUser();
+  },
 };
 </script>
